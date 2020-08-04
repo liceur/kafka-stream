ea9f8d8066730e6246921e764bdd2f1cc32e7a57
Adrian Nicolas <liceurtwo@gmail.com>
2020-08-03 20:21:54 -0400

    Enable idempotence and proccesing guarantee

--- src/main/java/com/course/kafka/config/KafkaStreamConfig.java
+++ src/main/java/com/course/kafka/config/KafkaStreamConfig.java
@@ -1,5 +1,6 @@
 package com.course.kafka.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
@@ -22,8 +23,12 @@ public class KafkaStreamConfig {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
         // Not recommend to commit more frequently
         props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "3000");
+
 
         return new KafkaStreamsConfiguration(props);
     }
